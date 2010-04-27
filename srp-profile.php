b23b6111a2b432b44b713194463a72c4320ce415
maxchirkov <maxchirkov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-27 19:53:33 +0000

    Committing version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/simple-real-estate-pack-4/trunk@234520 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ srp-profile.php
@@ -0,0 +1,420 @@
+<?php
+/*---------------------------------------------*
+** Simple Realty Pack support functions
+** to work with Great Real Estate Plugin
+** Author: Max Chirkov
+** WebSite: www.PhoenixHomes.com
+**---------------------------------------------*/
+
+$srp_widgets = new srpWidgets();
+
+function srp_prepare_widgets_object(){
+    global $srp_widgets;
+
+    $init = array();
+    $init = apply_filters('srp_prepare_widgets_object', $init);
+
+    if(!is_array($init))
+        return;
+    foreach($init as $array){
+        foreach($array as $k => $v){
+            $$k = $v;            
+        }
+        $srp_widgets->add($name, $title, $tab_name, $content, $callback_function, $init_function, $ajax, $save_to_buffer);
+        foreach($array as $k => $v){            
+            unset($$k);
+        }
+    }    
+}
+
+
+if($_POST['srp_listing_values']){
+    $srp_property_values = $_POST['srp_listing_values'];
+}
+
+function srp_listing_content(){
+    do_action('srp_listing_content');
+}
+
+function _check_required_values(){
+    global $srp_property_values;
+    
+    if(!is_array($srp_property_values))
+        return;
+    
+    $requierd = array ('lat', 'lng', 'address', 'city', 'state', 'zip_code');
+    $keys = array_keys($srp_property_values);
+    foreach($requierd as $k){
+        if(!in_array($k, $keys))
+                return false;
+    }
+    return true;
+}
+
+
+if(!$srp_ext_gre_options = get_option('srp_ext_gre_options')) return;
+$srp_ext_gre_content = array_keys($srp_ext_gre_options['content']);
+$srp_ext_gre_tabs = $srp_ext_gre_options['tabs'];
+$srp_general_options = get_option('srp_general_options');
+
+function srp_gre_admin_scripts(){
+	if($g_api = get_option('greatrealestate_googleAPIkey')){		
+		echo "\n" . '<script type="text/javascript">
+//<![CDATA[
+		var srp_geo = "'. $g_api . '/";
+//]]>
+		</script>'. "\n";
+		echo '<script type="text/javascript" src="'.SRP_URL.'/js/srp-gre-admin.js"></script>';
+	}
+}
+add_action('admin_footer', 'srp_gre_admin_scripts');
+
+function srp_gre_content_init($init){
+    global $srp_widgets;
+
+    $gre_functions = array(
+        //'Description' => 'the_listing_description_content',
+        'Photos' => 'the_listing_gallery_content',
+        'Video' => 'the_listing_video_content',
+        'Panorama' => 'the_listing_panorama_content',
+        'Downloads' => 'the_listing_downloads_content',
+        'Community' => 'the_listing_community_content',
+    );    
+    foreach($gre_functions as $tab_name => $callback){
+        //if(srp_buffer($callback)){
+        if(function_exists($callback) && srp_buffer($callback)){
+            $init[] = array(
+                'name' => strtolower($tab_name),
+                'title'  => NULL, //GRE already provides H2 headings
+                'tab_name' => $tab_name,
+                'callback_function' => $callback,
+                'ajax' => false,
+                'save_to_buffer' => true,
+                );
+        }
+    }
+    return $init;
+}
+if(function_exists('greatrealestate_init')){
+    add_filter('srp_prepare_widgets_object', 'srp_gre_content_init', 2);
+}
+
+/*
+ * @args - ajax, tabs = bool
+ */
+function srp_profile($args = array()){
+    global $srp_general_options, $srp_widgets, $srp_property_values, $srp_ext_gre_content;
+    
+    if(count($srp_property_values) < 6)
+        return;
+
+    if(empty($args)){
+        $args['tabs'] = $srp_general_options['content']['srp_profile_tabs'];
+        $args['ajax'] = $srp_general_options['content']['srp_profile_ajax'];
+    }
+
+    srp_prepare_widgets_object();
+    //var_dump($srp_widgets);
+    $js_func = 'srp_profile';
+    $content = '<div id="srp-tab-wrap">';
+    if($args['tabs']){        
+        $content .= $srp_widgets->get_tabs();
+        $js_func = 'srp_profile_tabs';
+    }
+    if(!get_option('srp_ext_gre_options')){
+            $content .=  '<div style="background:red; color: white; font-weight: bold; padding: 10px;">Please visit the <a href="'.ADMIN_URL.'/admin.php?page=srp_ext_gre">Extension to GRE settings</a> page to complete the installation.</div>';
+    }elseif($args['ajax']){
+        
+        foreach($srp_widgets->widgets as $widget){
+            if($widget->ajax == true){
+                if(in_array($widget->name, $srp_ext_gre_content)){
+                    $callbacks[] = '\'' . $widget->init_function . '\'';
+                }
+            }
+        }
+
+        if(is_array($callbacks)){
+
+            //check if any functions exist that use 3rd party APIs and require desclamers in the footer
+           /*
+            * ToDo Max: add filter to wp_footer to add disclamers.
+            * Don't loop an array, but rather do each instance separately since I have to check for their API keys.
+            *
+            $apis = array(
+                '' => 'srp_zillow_disclaimer',
+                '' => 'srp_rentometer_disclaimer',
+                '' => 'srp_Education_disclaimer',
+            );
+            */
+            $callbacks_string = implode(',', $callbacks);
+            $content .=  '<script type="text/javascript">
+                        var srp_listing_values = {' . "\n";
+            $i = 0;
+            $n = count($srp_property_values);
+            foreach($srp_property_values as $k => $v){
+                $i++;
+                if($i == $n) { $comma = ''; }else{ $comma = ','; }
+                $content .=  "\t\t\t" . $k . ': \''.$v.'\''.$comma . "\n";
+            }
+            
+                $content .=  "\t\t" .'};
+                    var load_srp_functions = ['. $callbacks_string .'];
+                    addLoadEvent('. $js_func .');
+                  </script>
+                  ';
+        }
+                    
+        $content .=  '<div id="srp_extension">'. $srp_widgets->get_all_ajax(false)  . '</div>';
+    }else{        
+        $content .= '<div id="srp_extension">';
+        $content .= $srp_widgets->get_all();
+        $content .= '</div>';
+    }
+    $content .= '</div>';
+
+    echo $content;
+}
+
+/*---------------------------------------------*
+** Substitute function the_listing_map_content()
+** to be placed in listingpage.php template
+** for Great Real Estate Plugin
+**---------------------------------------------*/
+
+function srp_gre_the_listing_map_content() {
+    global $srp_widgets, $srp_ext_gre_content, $srp_ext_gre_tabs, $srp_property_values;
+	if (!_check_required_values()) return;
+
+        $title = 'Location Map';
+        $content = '<div class=\"srp-tabs\">';
+        $content .= srp_map($srp_property_values['lat'], $srp_property_values['lng'], $srp_property_values['html']);
+        $content .= '</div>';
+        return $content;
+        //$srp_widgets->add('map', $title, $content);
+}
+function srp_map_content_init($init){
+    $array = array(
+            'name' => 'map',
+            'title'  => 'Location Map',
+            'tab_name' => 'Map',
+            'callback_function' => 'srp_gre_the_listing_map_content',
+            'init_function' => __FUNCTION__,
+            'ajax' => false,
+            //'save_to_buffer' => false,
+            );
+    $init[] = $array;
+    return $init;
+}
+add_filter('srp_prepare_widgets_object', 'srp_map_content_init', 1);
+
+function srp_gre_the_listing_schools_content(){
+	global $srp_widgets, $srp_ext_gre_content, $srp_ext_gre_tabs, $srp_property_values;
+
+	if(!in_array('schools', $srp_ext_gre_content)) return;
+	if(!EDU_API_KEY) return;
+	if(!isset($srp_property_values['lat']) || !isset($srp_property_values['lng'])){ return; }
+
+	$address = $srp_property_values['address'] . ' ' . $srp_property_values['city'] . ', ' . $srp_property_values['state'] . ' ' . $srp_property_values['zip_code'];
+	if (!($content = srp_schoolSearch_shortcode(array("lat"=>$srp_property_values['lat'],  "lng"=>$srp_property_values['lng'], "distance"=>3, "groupby"=>"gradelevel", "output"=>"table", "location_title"=>$address)))) return;
+
+        return $content;
+}
+function srp_schools_content_init($init){
+    global $srp_ext_gre_tabs;
+    if (!EDU_API_KEY) return $init;
+    $array = array(
+            'name' => 'schools',
+            'title'  => 'Local Schools',
+            'tab_name' => 'Schools',
+            'callback_function' => 'srp_gre_the_listing_schools_content',
+            'init_function' => __FUNCTION__,
+            'ajax' => true,
+            );
+    $init[] = $array;
+    return $init;
+}
+add_filter('srp_prepare_widgets_object', 'srp_schools_content_init');
+/*-------------------------------------------------------------------------*
+** Market Trends Tab (by Trulia) to be placed in listingpage.php template
+**-------------------------------------------------------------------------*/
+function srp_gre_the_trulia_stats_content(){
+	global $srp_widgets, $srp_ext_gre_content, $srp_ext_gre_tabs, $srp_property_values;
+	if(!in_array('trulia_stats', $srp_ext_gre_content)) return;
+	
+	if(!function_exists('srp_get_trulia_stats')) return;
+	global $graph_types;
+	foreach($graph_types as $k => $v){
+		$id = str_replace(' ', '_', strtolower($k));
+		$output .='<div id="'.$id.'">' . srp_get_trulia_stats(array('type' => $k, 'city' => $srp_property_values['city'], 'state' => $srp_property_values['state'], 'zipcode' => $srp_property_values['zip_code'])) . '</div>';
+		$li .= '<li><a href="#'.$id.'">'.$v.'</a></li>'."\n";
+	}
+	$content = "<div class=\"srp-tabs\"><ul class=\"clearfix\">\n $li \n </ul>\n";
+	$content .= $output . '</div>';
+
+        return $content;
+}
+function srp_trulia_stats_content_init($init){
+    global $srp_ext_gre_tabs, $srp_ext_gre_content;    
+
+    $array = array(
+            'name' => 'trulia_stats',
+            'title'  => 'Market Statistics',
+            'tab_name' => 'Market Stats',
+            'callback_function' => 'srp_gre_the_trulia_stats_content',
+            'init_function' => __FUNCTION__,
+            'ajax' => true,
+            );
+    $init[] = $array;
+    return $init;
+}
+add_filter('srp_prepare_widgets_object', 'srp_trulia_stats_content_init');
+/*-------------------------------------------------------------------------*
+** Market Trends Tab (by ALTOS Research) to be placed in listingpage.php template
+**-------------------------------------------------------------------------*/
+function srp_gre_the_altos_stats_content($width = 600, $height = 400){
+	global $srp_widgets, $srp_ext_gre_content, $srp_ext_gre_tabs, $srp_property_values;
+
+	if(!function_exists('srp_get_altos_stats')) return;
+	global $metrics, $pricerangequartile, $rollingaverage;
+	foreach($metrics as $k => $v){
+		$id = str_replace(' ', '_', strtolower($k));
+		$output .='<div id="'.$id.'">' . srp_get_altos_stats(array('type' => $k, 'width' => $width, 'height' => $height, 'city' => $srp_property_values['city'], 'state' => $srp_property_values['state'], 'zipcode' => $srp_property_values['zip_code'])) . '</div>';
+		$li .= '<li><a href="#'.$id.'">'.$v.'</a></li>'."\n";
+	}
+	$content = "<div class=\"srp-tabs\"><ul class=\"clearfix\">\n $li \n </ul>\n";
+	$content .= $output . '</div>';
+
+        return $content;
+}
+function srp_altos_stats_content_init($init){
+    global $srp_ext_gre_tabs, $srp_ext_gre_content;
+
+    $array = array(
+            'name' => 'altos_stats',
+            'title'  => 'Market Statistics',
+            'tab_name' => 'Market Stats',
+            'callback_function' => 'srp_gre_the_altos_stats_content',
+            'init_function' => __FUNCTION__,
+            'ajax' => true,
+            );
+    $init[] = $array;
+    return $init;
+}
+add_filter('srp_prepare_widgets_object', 'srp_altos_stats_content_init');
+/*-------------------------------------------------------------------------*
+** Mortgage/Financing Tab to be placed in listingpage.php template
+**-------------------------------------------------------------------------*/
+function srp_gre_the_mortgage_content(){
+	global $srp_widgets, $srp_ext_gre_content, $srp_ext_gre_tabs, $srp_property_values;
+	if(!in_array('mortgage_calc', $srp_ext_gre_content) && !in_array('closing_estimator', $srp_ext_gre_content) && !in_array('affordability_calc', $srp_ext_gre_content) && !in_array('rental_meter', $srp_ext_gre_content)) return;
+	
+        if(in_array('mortgage_calc', $srp_ext_gre_content)){
+            $content = '
+		<div style="float: left; width: 50%;">'
+			. srp_MortgageCalc_shortcode(array('price_of_home'=> $srp_property_values['listing_price']))
+		. '</div>';
+        }
+	
+        $content .= '<div style="float:left; width: 50%;">';
+			
+	if(in_array('closing_estimator', $srp_ext_gre_content)){
+            $content .= srp_ClosingCosts_shortcode(array('loan_amount'=>$srp_property_values['listing_price']));
+        }
+
+        $content .= '</div>
+		<div style="float:left; width: 50%;">';
+
+        if(in_array('affordability_calc', $srp_ext_gre_content)){
+            $content .= srp_AffordabilityCalc_shortcode();
+        }
+
+        $content .= '
+		</div>
+		<div style="float:left; width: 50%;">';
+
+        if(in_array('rental_meter', $srp_ext_gre_content)){
+            $content .= srp_RentMeter_shortcode(array('citystatezip'=>$srp_property_values['zip_code'], 'beds'=>$srp_property_values['bedrooms']));
+	}			
+	
+        $content .= '</div>';
+
+        return $content;
+}
+
+function srp_mortgage_content_init($init){
+    global $srp_ext_gre_tabs, $srp_ext_gre_content;    
+
+    $array = array(
+            'name' => 'financial',
+            'title'  => 'Financial Tools',
+            'tab_name' => 'Financing',
+            'callback_function' => 'srp_gre_the_mortgage_content',
+            'init_function' => __FUNCTION__,
+            'ajax' => true,
+            );
+    $init[] = $array;
+    return $init;
+}
+add_filter('srp_prepare_widgets_object', 'srp_mortgage_content_init');
+
+/*-------------------------------------------------------------------------*
+** Yelp Tab to be placed in listingpage.php template
+**-------------------------------------------------------------------------*/
+function srp_gre_the_yelp_content(){
+	global $srp_widgets, $srp_ext_gre_content, $srp_ext_gre_tabs, $srp_property_values;
+	if(!in_array('yelp', $srp_ext_gre_content)) return;
+	
+	if (!get_option('srp_yelp_api_key')) return;
+	if(!function_exists('srp_Yelp_shortcode')) return;
+	if (!($content = srp_Yelp_shortcode($atts=array("lat"=>$srp_property_values['lat'], "lng"=>$srp_property_values['lng'], 'radius' => 3, 'output' => 'table', 'sortby' => 'distance', 'term' => null, 'num_biz_requested' => null, 'ajax' => null)))) return;
+
+        return $content;
+}
+function srp_yelp_content_init($init){
+    global $srp_ext_gre_tabs, $srp_ext_gre_content;    
+
+    if (!get_option('srp_yelp_api_key')) return $init;
+    
+    $array = array(
+            'name' => 'yelp',
+            'title'  => 'Businesses in the Neighborhood',
+            'tab_name' => 'Nearby Businesses',
+            'callback_function' => 'srp_gre_the_yelp_content',
+            'init_function' => __FUNCTION__,
+            'ajax' => true,
+            );
+    $init[] = $array;
+    return $init;
+}
+add_filter('srp_prepare_widgets_object', 'srp_yelp_content_init');
+/*-------------------------------------------------------------------------*
+** Walkscore Tab to be placed in listingpage.php template
+**-------------------------------------------------------------------------*/
+function srp_gre_the_walkscore_content(){
+	global $srp_widgets, $srp_ext_gre_content, $srp_ext_gre_tabs, $srp_property_values;
+	if(!in_array('walkscore', $srp_ext_gre_content)) return;
+	if (!$ws_wsid = get_option('srp_walkscore_api_key')) return;
+	$ws_address = $srp_property_values['address'] . ' ' . $srp_property_values['city'] . ', ' . $srp_property_values['state'] . ' ' . $srp_property_values['zip_code'];
+	
+	$content = srp_walkscore($ws_wsid, $ws_address, $ws_width=500, $ws_height=286, $ws_layout = 'horizontal');
+
+        return $content; //$ws_wsid . ' ' . $ws_address . ' ' . $ws_width=500 . ' ' . $ws_height=286 . ' ' . $ws_layout = 'horizontal';
+}
+function srp_walkscore_content_init($init){
+    global $srp_ext_gre_tabs, $srp_ext_gre_content;    
+
+    if (!$ws_wsid = get_option('srp_walkscore_api_key')) return $init;
+
+    $array = array(
+            'name' => 'walkscore',
+            'title'  => 'Walkability of the Neighborhood',
+            'tab_name' => 'Walkability',
+            'callback_function' => 'srp_gre_the_walkscore_content',
+            'init_function' => __FUNCTION__,
+            'ajax' => true,
+            );
+    $init[] = $array;
+    return $init;
+}
+add_filter('srp_prepare_widgets_object', 'srp_walkscore_content_init');
+?>
---
+++ templates/listing_page.php
@@ -0,0 +1,124 @@
+<?php if (function_exists('get_listing_status')) { ?>
+	<?php getandsetup_listingdata(); ?>
+<div id="srp_listing_details">
+    <div class="clearfix">
+    <div class="listing-slideshow span-10">
+            <div class="box">
+            <?php echo nggShowSlideshow(get_listing_galleryid(), $width='356', $height='267') ?>
+            </div>
+    </div>
+    <div class="page-propdata-box span-5 last">
+            <div class="box">
+            <h2><span>Property Details</span></h2>
+            <?php
+                    $details = array();
+            ?>
+
+            <div class="page-blurb"><?php the_listing_blurb(); ?></div>
+            <?php
+                    if ($bedrooms = get_listing_bedrooms())
+                            $details[] = "$bedrooms Bedrooms";
+                    if ($bathrooms = get_listing_bathrooms()) {
+                            $bath = "$bathrooms Full ";
+                    if ($halfbaths = get_listing_halfbaths())
+                            $bath .= "&amp; $halfbaths Half ";
+                            $bath .= " Baths";
+                  }
+                            $details[] = $bath;
+                    if ($garage = get_listing_garage())
+                            $details[] = "$garage Garage Spaces";
+                    if ($acsf = get_listing_acsf())
+                            $details[] = "$acsf Sq/Ft Under Air";
+                    if ($totsf = get_listing_totsf())
+                            $line2 .= "$totsf Sq/Ft Total";
+                            $acres = get_listing_acres();
+                    if ($acres > 0)
+                            $details[] = "$acres Acres";
+                    if (get_listing_haspool())
+                            $details[] = "Private Pool";
+                    if (get_listing_haswater())
+                            $details[] = "Waterfront";
+                    if (get_listing_hasgolf())
+                            $details[] = "On Golf Course";
+                    if (!empty($details)) { ?>
+          <div class='propdata'>
+                    <ul>
+            <?php
+                    $i = 0;
+                    foreach($details as $line){
+                            $i++;
+                            if($i&1){ $class = 'class="odd"'; }else{$class = 'class="even"';}
+                            print "<li $class>$line</li>";
+
+                    }
+            ?>
+                    </ul>
+                    <h4><?php the_listing_status(); ?>
+                    <?php if (get_listing_hasclosed()) { ?>
+                    <?php the_listing_saledate(); ?> for <?php the_listing_saleprice(); ?> - last offered<?php } else { ?>- Offered<?php } ?>  at <?php the_listing_listprice(); ?></h4>
+          </div>
+            </div>
+    </div>
+    </div>
+            <?php } ?>
+    <?php the_listing_description_content(); ?>
+
+    <?php } else { ?>
+    <?php //the_content(); // plugin disabled, just spit out the normal content ?>
+    <?php } ?>
+
+    <?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+
+
+    <?php if (function_exists('get_listing_status')) { ?>
+    <div id="listing-container">
+
+        <?php
+            /* Begin SRP Template Code */
+            $srp_property_values = array(
+                'lat' => get_listing_latitude(),
+                'lng' => get_listing_longitude(),
+                'address' => get_listing_address(),
+                'city' => get_listing_city(),
+                'state' => get_listing_state(),
+                'zip_code' => get_listing_postcode(),
+                'listing_price' => get_listing_listprice(),
+                'bedrooms'  => get_listing_bedrooms(),
+                'bathrooms' => get_listing_bathrooms(),
+                'html' => '<div style="text-align: center;">' . get_listing_thumbnail() . '</div><div style="text-align: center;font-size: 14px;line-height: normal;"><strong>' . get_listing_listprice() . '</strong></div><p style="text-align: center;font-size: 12px;line-height: normal;">' . get_listing_address() . ',<br />' . get_listing_city() . ' ' . get_listing_state() . ' ' . get_listing_postcode() . '</p>',
+
+            );
+
+            if(function_exists('srp_profile')){
+                srp_profile();
+            }else{
+                ?>
+                <ul id="tabnav">
+                <?php the_listing_map_tab(); // recommend this be first ?>
+                <?php //the_listing_description_tab(); ?>
+                <?php //the_listing_gallery_tab(); ?>
+                <?php the_listing_video_tab(); ?>
+                <?php the_listing_panorama_tab(); ?>
+                <?php the_listing_downloads_tab(); ?>
+                <?php the_listing_community_tab(); ?>
+                </ul>
+                <?php the_listing_map_content(); // recommend this be first ?>
+                <?php the_listing_description_content(); ?>
+                <?php the_listing_gallery_content(); ?>
+                <?php the_listing_video_content(); ?>
+                <?php the_listing_panorama_content(); ?>
+                <?php the_listing_downloads_content(); ?>
+                <?php the_listing_community_content();
+            }
+                    /* End SRP Template Code */
+            ?>
+
+            <?php
+            if(function_exists('insert_form')){
+                print '<a name="more_info"></a><h2><span>Request More Information</span></h2>';
+                insert_cform('Request More Information');
+            }
+            ?>
+    <?php } ?>
+    </div>
+</div>
